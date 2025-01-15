0280114e6ef1bdda16b8bce48b1c9b863034f819
Moosa <moosajamel.21@gmail.com>
2025-01-15 10:07:32 +0300

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router'
+import Nav from './components/Nav'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
+import Feed from './pages/Feed'
+import Home from './pages/Home'
+import './App.css'
+import { CheckSession } from './services/Auth'
+import { useEffect } from 'react'
+
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <Nav user={user} handleLogOut={handleLogOut} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/feed" element={<Feed user={user} />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
