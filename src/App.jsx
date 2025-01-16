c0591453510b5c24ae30f65c610720bfbbadada1
Moosa <moosajamel.21@gmail.com>
2025-01-16 10:38:50 +0300

    Some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn setUser={setUser} />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/feed" element={<Feed user={user} />} />
         </Routes>
       </main>
     </div>
