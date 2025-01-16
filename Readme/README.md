415ca21d2d39043cfbac107c698efacef03c0b67
Moosa <moosajamel.21@gmail.com>
2025-01-16 11:40:15 +0300

    Home page

---
+++ Readme/README.md
@@ -0,0 +1,94 @@
+# RENT ORA
+
+![OurWeb](./Home.png)
+
+## Background:
+
+The website serves as a dynamic platform connecting property owners and renters. Users can browse through a wide range of available properties for rent, complete with detailed descriptions and images. Property owners have the option to list their properties on the platform, making them accessible to potential renters.
+
+Renters can select properties based on their preferred dates and view the current booking status of each property. After renting a property, users can leave reviews in the comments section, sharing their experiences with others. This comprehensive functionality ensures a seamless and interactive experience for both renters and property owners.
+
+## User Stories
+
+1. **View Properties**  
+   As a user, I want to browse through the available properties with detailed descriptions, images, and booking statuses so that I can easily find a property that meets my needs.
+
+2. **Add My Own Properties**  
+   As a user, I want to list my own properties on the platform, including details and images, so that other users can view and rent them.
+
+3. **Edit Profile**  
+   As a user, I want to update my profile information, such as my name, password, and profile picture, so that my account reflects my current details.
+
+4. **Add Reviews**  
+   As a user, I want to leave a review for a property I have rented, so I can share my experience with other users.
+
+5. **View Booked Properties**  
+   As a user, I want to see a list of all the properties I have booked, along with the booking details, so that I can manage my reservations easily.
+
+6. **View Property Status**  
+   As a user, I want to see the current status of each property (e.g., available, booked, or under maintenance) so that I can make informed decisions while browsing.
+
+## Entity relationship diagrams (ERD)
+
+The data that will be utilised in an One method of describing the data that will be utilised in an application is using entity relationship diagrams, or ERDs. Based on the user stories we developed earlier, we are aware that we need to monitor users and listings. Before we look at the user-listing relationships, let's look at the information we need to monitor for each resource.
+
+### Our ERD:
+
+![erd](./erd.jpeg)
+
+# wireframe:
+
+![wireFrame](./wireframe.png)
+
+## Lead page:
+
+![Leadpage](./Lead.png)
+
+## SignUp:
+
+![SignUp](./SignUp.png)
+
+## Home page:
+
+![Home](./Home.png)
+
+## SignIn page:
+
+![SignIn](./SignIn.png)
+
+## My Property:
+
+![MyProperty](./MyProperty.png)
+
+## Offers Property:
+
+![Offers](./Offers.png)
+
+## User Profile:
+
+![Profile](./Profile.png)
+
+## Book:
+
+![Book](./Book.png)
+
+## Details:
+
+![Details](./Details.png)
+
+[wireFrame](https://www.canva.com/design/DAGcPlfYAFM/sZZUWBEjSTZsFIqC7lz64A/edit?utm_content=DAGcPlfYAFM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
+
+## Technologies Used
+
+- **React**: JavaScript library for building user interfaces.
+- **React Router**: For page navigation.
+- **CSS**: For styling the app (or you can use CSS frameworks like Bootstrap or Material UI).
+- **useState** and **useEffect**: For state management and handling side effects.
+- **Node.js**: JavaScript runtime for building the backend server.
+- **Express**: Web framework for Node.js to handle API requests and manage routes.
+
+# Future Fetchers:
+
+1. Payment get way
+2. Integrate ai
+3. Add more registration options
--- src/pages/PropertiesPage.jsx
+++ src/pages/PropertiesPage.jsx
@@ -1,89 +1,47 @@
 import { useEffect, useState } from 'react'
 import CategoryDropdown from '../components/CategoryDropdown'
-import { getProductsByCategory } from '../services/ProductServices'
-import { getProducts, deleteProduct } from '../services/ProductServices'
-import { addToCart } from '../services/CartServices'
+import {
+  getPropertiesByCategory,
+  GetProperties,
+  DeleteProperty
+} from '../services/PropertyServices'
+import { PlaceBooking } from '../services/BookServices'
 import { useNavigate, Link } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const PropertiesPage = ({ user }) => {
-  const [products, setProducts] = useState([])
+  const [properties, setProperties] = useState([])
   const [selectedCategory, setSelectedCategory] = useState('')
   const [error, setError] = useState(null)
   const navigate = useNavigate()
-  const [quantities, setQuantities] = useState({})
 
   useEffect(() => {
-    const fetchProducts = async () => {
+    const fetchProperties = async () => {
       try {
         let data
         if (selectedCategory) {
-          console.log(`Fetching products for category: ${selectedCategory}`)
-          data = await getProductsByCategory(selectedCategory)
+          console.log(`Fetching properties for category: ${selectedCategory}`)
+          data = await getPropertiesByCategory(selectedCategory)
         } else {
-          console.log('Fetching all products')
-          data = await getProducts()
+          console.log('Fetching all properties')
+          data = await GetProperties()
         }
-        console.log('Fetched products:', data)
-        setProducts(data)
+        setProperties(data)
       } catch (err) {
-        console.error('Error fetching products:', err)
-        toast.error('Failed to fetch products.')
+        console.error('Error fetching properties:', err)
+        toast.error('Failed to fetch properties.')
       }
     }
 
-    fetchProducts()
+    fetchProperties()
   }, [selectedCategory])
-
-  const handleAddToCart = async (productId, quantity, price, discount = 0) => {
-    try {
-      const products = [
-        {
-          product: productId,
-          quantity: quantity,
-          price: price,
-          discount: discount
-        }
-      ]
-      await addToCart(products)
-      setQuantities((prevQuantities) => ({
-        ...prevQuantities,
-        [productId]: 0
-      }))
-      toast.success('Product added to cart successfully!')
-    } catch (err) {
-      console.error('Error adding product to cart:', err)
-      toast.error('Failed to add product. Please try again.')
-    }
-  }
-
-  const handleDelete = async (productId) => {
-    try {
-      await deleteProduct(productId)
-      toast.success('Product deleted successfully!')
-      setProducts((prevProducts) =>
-        prevProducts.filter((product) => product._id !== productId)
-      )
-    } catch (err) {
-      console.error('Error details:', err.response?.data || err.message)
-      toast.error('Failed to delete product. Please try again.')
-    }
-  }
-
-  const handleQuantityChange = (productId, event) => {
-    const newQuantity = Math.max(1, event.target.value)
-    setQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [productId]: newQuantity
-    }))
-  }
 
   return (
     <div>
       <ToastContainer />
-      <div className="browse-products-header">
-        <h1>Browse Products</h1>
+      <div className="browse-properties-header">
+        <h1>Browse Properties</h1>
       </div>
 
       <CategoryDropdown
@@ -91,86 +49,32 @@ const PropertiesPage = ({ user }) => {
         onCategoryChange={setSelectedCategory}
       />
 
-      <div className="products-grid">
+      <div className="properties-grid">
         {error && <p className="error-message">{error}</p>}
-        {products && products.length > 0 ? (
-          products.map((product) => (
-            <div key={product._id} className="product-card">
-              <Link to={`/product/${product._id}`}>
+        {properties && properties.length > 0 ? (
+          properties.map((property) => (
+            <div key={property._id} className="property-card">
+              <Link to={`/property/${property._id}`}>
                 <img
-                  src={product.imageUrl}
-                  alt={product.name}
-                  className="product-image"
+                  src={property.imageUrl}
+                  alt={property.title}
+                  className="property-image"
                 />
-              </Link>
-              <h2>{product.name}</h2>
+                <h2>{property.title}</h2>
+                <p>Location: {property.location}</p>
+                <p>Price: ${property.price}</p>
 
-              <p>Price: ${product.discountedPrice}</p>
-
-              {product.stockQuantity === 0 ? (
-                <p className="out-of-stock">Out of Stock</p>
-              ) : (
-                !user?.isAdmin && (
-                  <div className="quantity-container">
-                    <label htmlFor={`quantity-${product._id}`}>Quantity:</label>
-                    <input
-                      type="number"
-                      id={`quantity-${product._id}`}
-                      name="quantity"
-                      min="1"
-                      max={product.stockQuantity}
-                      value={quantities[product._id] || 1}
-                      onChange={(e) => handleQuantityChange(product._id, e)}
-                      className="quantity-input"
-                    />
-                  </div>
-                )
-              )}
-
-              {product.stockQuantity > 0 && !user?.isAdmin && (
                 <button
-                  onClick={() => {
-                    const quantityInput = document.getElementById(
-                      `quantity-${product._id}`
-                    )
-                    const quantity = parseInt(quantityInput.value, 10)
-
-                    if (!quantity || quantity <= 0) {
-                      alert('Please enter a valid quantity.')
-                      return
-                    }
-
-                    handleAddToCart(product._id, quantity, product.price)
-                  }}
-                  className="cart-buttonp"
-                  aria-label={`Add ${product.name} to cart`}
+                  className="book-buttonp"
+                  aria-label={`Book ${property.title}`}
                 >
-                  Add to Cart
+                  Book Now
                 </button>
-              )}
-
-              {user?.isAdmin && (
-                <>
-                  <button
-                    onClick={() => navigate(`/edit-product/${product._id}`)}
-                    className="edit-buttonp"
-                  >
-                    Edit
-                  </button>
-
-                  <button
-                    type="button"
-                    onClick={() => handleDelete(product._id)}
-                    className="delete-buttonp"
-                  >
-                    Delete Product
-                  </button>
-                </>
-              )}
+              </Link>
             </div>
           ))
         ) : (
-          <p>No products available</p>
+          <p>No properties available</p>
         )}
       </div>
     </div>
